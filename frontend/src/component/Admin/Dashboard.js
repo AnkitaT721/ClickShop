e3d5cfab6c0dca690e290db362e0e90d593d122e
Ankita Talukdar <talukdarankita721@gmail.com>
2023-08-16 03:44:29 +0530

    changes 11

--- frontend/src/component/Admin/Dashboard.js
+++ frontend/src/component/Admin/Dashboard.js
@@ -4,7 +4,7 @@ import "./Dashboard.css";
 import { Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import { Line, Doughnut } from "react-chartjs-2";
-import { Chart as ChartJS, registerables } from 'chart.js';
+import { Chart as ChartJS, registerables } from "chart.js";
 import { useDispatch, useSelector } from "react-redux";
 import { getAdminProduct } from "../../actions/productAction";
 import MetaData from "../layout/MetaData";
@@ -12,92 +12,98 @@ import { getAllOrders } from "../../actions/orderAction";
 ChartJS.register(...registerables);
 
 const Dashboard = () => {
-
   const dispatch = useDispatch();
 
   const { products } = useSelector((state) => state.products);
-  const { orders } = useSelector((state) =>state.allOrders); 
-  const { users } = useSelector((state) =>state.allUsers); 
+  const { orders } = useSelector((state) => state.allOrders);
+  const { users } = useSelector((state) => state.allUsers);
 
-    let outOfStock = 0;
+  let outOfStock = 0;
 
-    products && products.forEach((item) => {
+  products &&
+    products.forEach((item) => {
       if (item.stock === 0) {
         outOfStock += 1;
       }
     });
 
-    let inStock = products.length - outOfStock;
+  let inStock = products.length - outOfStock;
 
-    useEffect(() => {
-      dispatch(getAdminProduct());
+  let totalAmount = 0;
+  orders &&
+    orders.forEach((item) => {
+      totalAmount += item.totalPrice;
+    });
 
-      dispatch(getAllOrders());
+  useEffect(() => {
+    dispatch(getAdminProduct());
+
+    dispatch(getAllOrders());
   }, [dispatch]);
 
-    const lineState = {
-        labels: ["Initial Amount", "Total Amount"],
-        datasets: [
-            {
-                label: "TOTAL AMOUNT",
-                backgroundColor: ["tomato"],
-                hoverBackgroundColor: ["rgb(197, 72, 49)"],
-                data: [0, 4000]
-            }
-        ]
-    };
+  const lineState = {
+    labels: ["Initial Amount", "Total Amount"],
+    datasets: [
+      {
+        label: "TOTAL AMOUNT",
+        backgroundColor: ["tomato"],
+        hoverBackgroundColor: ["rgb(197, 72, 49)"],
+        data: [0, totalAmount],
+      },
+    ],
+  };
 
-    const doughnutState = {
-        labels: ["Out of Stock", "In Stock"],
-        datasets: [
-            {
-                backgroundColor: ["#FFC5D4", "#FF5D89"],
-                hoverBackgroundColor: ["#4B5000", "#35014F"],
-                data: [outOfStock, inStock]
-            }
-        ]
-    };
+  const doughnutState = {
+    labels: ["Out of Stock", "In Stock"],
+    datasets: [
+      {
+        backgroundColor: ["#FFC5D4", "#FF5D89"],
+        hoverBackgroundColor: ["#4B5000", "#35014F"],
+        data: [outOfStock, inStock],
+      },
+    ],
+  };
 
   return (
     <>
-    <MetaData title="Dashboard" />
-    <div className="dashboard">
-      <Sidebar />
+      <MetaData title="Dashboard" />
+      <div className="dashboard">
+        <Sidebar />
 
-      <div className="dashboardContainer">
-        <Typography component="h1">Dashboard</Typography>
+        <div className="dashboardContainer">
+          <Typography component="h1">Dashboard</Typography>
 
-        <div className="dashboardSummary">
-          <div>
-            <p>
-              Total Amount <br /> ₹5000
-            </p>
+          <div className="dashboardSummary">
+            <div>
+              <p>
+                Total Amount <br /> ₹{totalAmount.toFixed(2)}
+              </p>
+            </div>
+            <div className="dashboardSummaryBox2">
+              <Link to="/admin/products">
+                <p>Products</p>
+                <p>{products && products.length}</p>
+              </Link>
+              <Link to="/admin/orders">
+                <p>Orders</p>
+                <p>{orders && orders.length}</p>
+              </Link>
+              <Link to="/admin/users">
+                <p>Users</p>
+                <p>{users && users.length}</p>
+              </Link>
+            </div>
           </div>
-          <div className="dashboardSummaryBox2">
-            <Link to="/admin/products">
-              <p>Products</p>
-              <p>{products && products.length}</p>
-            </Link>
-            <Link to="/admin/orders">
-              <p>Orders</p>
-              <p>{orders && orders.length}</p>
-            </Link>
-            <Link to="/admin/users">
-              <p>Users</p>
-              <p>{users && users.length}</p>
-            </Link>
+
+          <div className="lineChart">
+            <Line data={lineState} />
           </div>
-        </div>
 
-        <div className="lineChart">
-          <Line data={lineState} />
-        </div>
-
-        <div className="doughnutChart">
-          <Doughnut data={doughnutState} />
+          <div className="doughnutChart">
+            <Doughnut data={doughnutState} />
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
